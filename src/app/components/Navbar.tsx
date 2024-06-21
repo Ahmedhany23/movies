bf05c112a0cc20b8fc6048acd81e461761c3f79f
Ahmedhany23 <ahmedhany.22@hotmail.com>
2024-06-21 15:17:53 +0300

    commit on "master"

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,32 +1,36 @@
 "use client";
 import { useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation"; 
 import { Dosis } from "next/font/google";
 import { IoIosSearch } from "react-icons/io";
 import Link from "next/link";
+
 const inter = Dosis({ subsets: [] });
 
 export default function Navbar() {
-  const [searchParams]: any = useSearchParams();
+  const router = useRouter(); 
   const [inputValue, setInputValue] = useState("");
 
   function handleChangeInput(event: any) {
     setInputValue(event.target.value);
   }
 
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    router.push(`/pages/search?query=${inputValue}`); 
+  }
+
   return (
-    <nav className="py-3 z-10 relative ">
-      <div className="flex gap-[20px] flex-col lg:flex-row  lg:justify-between lg:items-center container mx-auto  ">
+    <nav className="py-3 z-10 relative">
+      <div className="flex gap-[20px] flex-col lg:flex-row lg:justify-between lg:items-center container mx-auto">
         <div>
           <Link href="/">
-            <h1
-              className={`text-[34px] text-[#f8f8f8] font-light ${inter.className}`}
-            >
+            <h1 className={`text-[34px] text-[#f8f8f8] font-light ${inter.className}`}>
               movie<strong className="font-semibold">pick</strong>
             </h1>
           </Link>
         </div>
-        <form className="mt-0 flex items-center text-center relative">
+        <form onSubmit={handleSubmit} className="mt-0 flex items-center text-center relative">
           <input
             onChange={handleChangeInput}
             value={inputValue}
@@ -34,12 +38,12 @@ export default function Navbar() {
             type="search"
             placeholder="Search a movie"
           />
-          <Link
+          <button
+            type="submit"
             className="min-h-[37px] text-center py-2 group-hover:bg-[var(--blue-dark)] bg-[var(--light-color)] hover:bg-[var(--blue-dark)] cursor-pointer duration-200 search h-full"
-            href={`/pages/search?query=${inputValue}`}
           >
-            <IoIosSearch className="text-[var(--white)] text-xl " />
-          </Link>
+            <IoIosSearch className="text-[var(--white)] text-xl" />
+          </button>
         </form>
       </div>
     </nav>
