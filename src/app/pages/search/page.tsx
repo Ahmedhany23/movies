c0b1b87d09958077034bca4088caf509f5666df6
Ahmedhany23 <ahmedhany.22@hotmail.com>
2024-06-21 15:31:22 +0300

    commit on "master"

--- src/app/pages/search/page.tsx
+++ src/app/pages/search/page.tsx
@@ -1,36 +1,9 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useSearchParams } from "next/navigation";
-import MoviesGrid from "@/app/components/MoviesGrid";
+import Loading from "@/app/components/Loading";
+import SearchByTitle from "@/app/components/SearchByTitle";
+import { Suspense } from "react";
 
 export default function Search() {
-  const [movies, setMovies] = useState<any[]>([]);
-  const searchParams = useSearchParams();
-  const query = searchParams.get('query') || '';
-
-  const getMoviesBySearch = async (query: string) => {
-    try {
-      const res = await axios.get(
-        `https://api.themoviedb.org/3/search/movie?api_key=52ef927bbeb21980cd91386a29403c78&language=en-US&query=${query}`
-      );
-      setMovies(res.data.results);
-    } catch (error) {
-      console.error("Failed to fetch movies:", error);
-    }
-  };
-
-  useEffect(() => {
-    if (query) {
-      getMoviesBySearch(query);
-    }
-  }, [query]);
-
   return (
-    <div className="container mx-auto mt:32 lg:mt-16">
-      <p className="text-2xl mb-5">Search Results</p>
-      <MoviesGrid movies={movies}/>
-    </div>
-  );
+    <Suspense fallback={<Loading/>}><SearchByTitle/></Suspense>
+  )
 }
